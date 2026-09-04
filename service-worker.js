f127255b6047ae3634f1e8e9646dd6e453eaa80e
maracuja344-cpu <maracuja344@gmail.com>
2026-09-04 19:38:32 +0300

    chore(pwa): bump release to v68

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const RELEASE_VERSION = "pwa-v67";
+const RELEASE_VERSION = "pwa-v68";
 const SHELL_CACHE = `pojidmusic-shell-${RELEASE_VERSION}`;
 const SDK_CACHE = "pojidmusic-sdk-supabase-2.112.2";
 const CACHE_PREFIX = "pojidmusic-";
-const RELEASE_MARKER = `<meta name="pojidmusic-release" content="pwa-v67">`;
+const RELEASE_MARKER = `<meta name="pojidmusic-release" content="pwa-v68">`;
 const SERVED_RELEASE_MARKER = `<meta name="pojidmusic-release" content="${RELEASE_VERSION}">`;
-const ENTRY_VERSION = "67";
+const ENTRY_VERSION = "68";
 
 const CRITICAL_SHELL_ASSETS = [
     "./index.html", "./style.css", "./telegram-profile.css", "./telegram-profile-v45.css", "./mobile-navigation.css", "./artist-mobile-list.css", "./track-management-surface.css", "./home-discovery.css", "./home-discovery.js", "./artist-public-surface.js", "./track-editor-modal-guard.js", "./album-upload.css", "./album-upload-mobile-compact.css", "./release-upload-chooser.css", "./track-upload-wizard.css", "./tracks.js", "./manifest.webmanifest", "./img/cover.jpg",
